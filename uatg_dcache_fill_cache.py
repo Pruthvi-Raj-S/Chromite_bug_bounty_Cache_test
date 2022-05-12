bb093d8e138793a01e41ac8d809d419f85a97d6b
Pruthvi-Raj-S <87191910+Pruthvi-Raj-S@users.noreply.github.com>
2022-05-13 00:51:38 +0530

    Update uatg_dcache_fill_cache.py

--- uatg_dcache_fill_cache.py
+++ uatg_dcache_fill_cache.py
@@ -6,51 +6,57 @@ import re
 import os
 import random	
 
-class uatg_cache_dcache_fill_buffer(IPlugin):
-    
+class uatg_cache_fillcache_01(IPlugin):
+    """
+    The test is used to fill cache. 
+    It uses consecutive load operations to do so.
+    """
+
     def __init__(self):
+
         super().__init__()
 
     def execute(self, core_yaml, isa_yaml):
-        _dcache_dict = core_yaml['dcache_config']
+ 
+        _dcache_dict = core_yaml['dcache_configuration']
         _dcache_en = _dcache_dict['instantiate']
         self._sets = _dcache_dict['sets']
         self._word_size = _dcache_dict['word_size']
         self._block_size = _dcache_dict['block_size']
         self._ways = _dcache_dict['ways']
-        self._cache_size=_dcache_dict['cache_size']
-        self._fb_size=_dcache_dict['fb_size']
+        self._cache_size=self._sets*self._ways*self._block_size
+        
         return True
-    
-    def check_log(self, log_file_path, reports_dir):
-        return None
-    
-    def generate_covergroups(self, config_file):
-        ''
-    
+
     def generate_asm(self) -> List[Dict[str, str]]:
-    
+        """
+        This method returns a string of the ASM file to be generated.
+        This ASM file is written as the ASM file which will be run on the DUT.
+        """
         asm_data = '\nrvtest_data:\n'
 
         for i in range (self._cache_size*4):
-        	asm_data+=f"\t.word 0x{str(hex(random.randrange(16**8))[2:]).zfill(8)}\n"
-            
-        asm='\n\tfence\n\tla t1,rvtest_data\t\n'
-        
-        for i in range(self._cache_size):
-	        asm+=f'\n\tlw t0, 0(t1)\n\tli a1, {self._sets*self._block_size*self._word_size}\n\taddi t1, t1, a1\n'
-        
-        asm+="end:\t\n\tnop\n"
+        	val=str(hex(int(random.uniform(0,self._cache_size*4)))[2:].zfill(8))
+        	asm_data+=f"\t.word 0x{val}\n"
 
-        # compile macros for the test
-        compile_macros=[]
+
+        asm='init:\n\tfence\n\tla t1, rvtest_data\t\n'
         
-        # return asm_code and sig_code
-        test_dict.append({
+        asm+='fill:'
+        for i in range(self._cache_size):
+	        asm+=f'\n\tlw t0, 0(t1)\n\taddi t1, t1, {self._sets*self._block_size*self._word_size}\n'
+        
+        asm+="end :\t\n\tnop\n"
+
+        return [{
             'asm_code': asm,
             'asm_data': asm_data,
             'asm_sig': '',
-            'compile_macros': compile_macros
-        })
-        
-        return test_dict
+            'compile_macros': []
+        }]
+
+    def check_log(self, log_file_path, reports_dir):
+        return None
+
+    def generate_covergroups(self, config_file):
+        return ''
